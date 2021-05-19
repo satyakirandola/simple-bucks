9cadfa821823438acc0d2b99ea8c63f8ec886383
satyakirandola <84455982+satyakirandola@users.noreply.github.com>
2021-05-19 21:35:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask,request
+app=Flask(__name__)
+@app.route("/send")
+def msg():
+  mail=request.args.get("mail")
+  amt=request.args.get("amt")
+  import telepot
+  bot=telepot.Bot("1810918140:AAEDH8WcSw3FVgI_93Yp9cJcTPUIZ58ODFg")
+  msg="hello"
+  bot.sendMessage(1113599535,msg)
+  return "200"
+if __name__=="__main__":
+  app.run()
