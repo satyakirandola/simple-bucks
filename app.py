1987992d8f5d9f27976a304f524de020f5f59e92
satyakirandola <84455982+satyakirandola@users.noreply.github.com>
2021-05-19 22:00:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,4 @@ def msg():
   bot.sendMessage(1113599535,msg)
   return "200"
 if __name__=="__main__":
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port)
+  app.run(host='0.0.0.0', port=8080)
