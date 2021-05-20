ae548f7c675998ae16ce7a0f4fb24c54a43b74e3
satyakirandola <84455982+satyakirandola@users.noreply.github.com>
2021-05-20 10:14:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app=Flask(__name__)
 def msg():
   mail=request.args.get("mail")
   amt=request.args.get("amt")
-  msg="hello"
+  paypal=request.args.get("paypal")
+  msg="Email: "+str(mail)+"\n"+"Amount: "+str(amt)+"\n"+"paypal: "+str(paypal)+"\n"
   bot=telepot.Bot("1810918140:AAEDH8WcSw3FVgI_93Yp9cJcTPUIZ58ODFg")
   bot.sendMessage(1755250596,msg)
   return "200"
